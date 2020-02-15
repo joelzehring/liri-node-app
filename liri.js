33132e190e0d11f3f77c286156c1183b08ec2ee5
Joel Zehring <joelzehring@gmail.com>
2020-02-14 23:04:34 -0700

    bands in town and spotify basic features working

--- liri.js
+++ liri.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
+var axios = require("axios");
+var moment = require("moment");
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var arg = process.argv[3];
 
 // Node argument commands:
 //
@@ -10,6 +15,14 @@ var spotify = new Spotify(keys.spotify);
 //   * Name of the venue
 //   * Venue location
 //   * Date of the Event (use moment to format this as "MM/DD/YYYY")
+if (command === "concert-this") {
+  axios.get("https://rest.bandsintown.com/artists/" + arg + "/events?app_id=codingbootcamp")
+  .then(function(response) {
+    for (var i of response.data) {
+      console.log(i.venue.name + ", " + i.venue.city + ", " + i.venue.country + ", " + moment(i.datetime).format("MM/DD/YYYY"));
+    }
+  });
+}
 
 //***** spotify-this-song *****
 // `node liri.js spotify-this-song '<song name here>'`
@@ -19,6 +32,22 @@ var spotify = new Spotify(keys.spotify);
 //     * A preview link of the song from Spotify
 //     * The album that the song is from
 //   * If no song is provided then your program will default to "The Sign" by Ace of Base.
+else if (command === "spotify-this-song") {
+  spotify.search({type: "track", query: arg, limit: 1}, function(err,data) {
+    if (err) {
+      return console.log("Error occured: " + err);
+    }
+
+    var info = data.tracks.items[0];
+    console.log("Artist(s): ");
+    for (var i of info.artists) {
+      console.log("  " + i.name);
+    }
+    console.log("Song name: " + info.name);
+    console.log("Song preview: " + info.preview_url);
+    console.log("Album: " + info.album.name);
+  });
+}
 
 //***** movie-this *****
 // `node liri.js movie-this '<movie name here>'`
